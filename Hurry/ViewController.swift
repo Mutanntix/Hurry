daf3a26a94f6a6492d31e401fd7a9927d49c671e
Mutanntix <muradze96@icloud.com>
2022-02-21 15:56:28 +0300

    Initial Commit

---
+++ Hurry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hurry
+//
+//  Created by Мурад on 21.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
