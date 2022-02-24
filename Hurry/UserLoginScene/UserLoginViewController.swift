318940aad73747909d1de986e6dee885b885e9da
Mutanntix <muradze96@icloud.com>
2022-02-24 20:31:37 +0300

    Just added UIStackView and some subViews for it

--- Hurry/UserLoginScene/UserLoginViewController.swift
+++ Hurry/UserLoginScene/UserLoginViewController.swift
@@ -17,14 +17,16 @@ class UserLoginViewController: UIViewController, UITextFieldDelegate {
     let loginButton = UIButton()
     let accountOwningButton = UIButton()
     let forgotPasswordButton = UIButton()
+    let userLoginStackView = UIStackView()
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         initializate()
         setupConstraints()
-        print(self.view.frame.width)
+        
     }
 
     private func initializate() {
@@ -32,19 +34,17 @@ class UserLoginViewController: UIViewController, UITextFieldDelegate {
         self.navigationItem.hidesBackButton = true
         
         setupMainView()
-        setupLabels()
-        setupTextFields()
+        setupStackView()
         
         view.addSubview(mainView)
-
-        mainView.addSubview(loginTextField)
-        mainView.addSubview(passwordTextField)
-        mainView.addSubview(loginLabel)
-    
-
+        
+        mainView.addSubview(userLoginStackView)
     }
     
     private func setupConstraints() {
+        let width = view.frame.width
+        let height = view.frame.height
+        
         // mainView contstraints
         mainView.snp.makeConstraints { make in
             let height = self.view.frame.size.height * 0.8
@@ -56,32 +56,49 @@ class UserLoginViewController: UIViewController, UITextFieldDelegate {
             make.centerX.equalToSuperview()
         }
         
-        loginTextField.snp.makeConstraints { make in
-            let width = self.mainView.frame.width
-            let height = self.mainView.frame.size.height
+        userLoginStackView.snp.makeConstraints { make in
+
+            if view.frame.size.height <= 667 {
+                make.height.equalTo(height * 0.2)
+            } else { make.height.equalTo(height * 0.15) }
             
-            make.height.equalTo(40)
-            make.width.equalTo(150)
-            make.top.equalToSuperview().inset(50)
-            make.left.equalToSuperview().inset(50)
-        }
-        
-        passwordTextField.snp.makeConstraints { make in
-            let width = self.mainView.layer.frame.width * 0.8
-            
-            make.height.equalTo(40)
-            print(width)
-            make.width.equalTo(150)
-            make.top.equalToSuperview().inset(0)
-            make.left.equalToSuperview().inset(0)
-        }
-        
-        loginLabel.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
+            make.width.equalTo(width * 0.75)
             make.centerX.equalToSuperview()
-            make.height.equalTo(150)
-            make.width.equalTo(60)
+            make.top.equalToSuperview().inset((height * 0.8) / 1.8)
         }
+     
+        loginTextField.snp.makeConstraints { make in
+            make.height.equalTo(40)
+            make.width.equalTo(width * 0.7)
+            make.centerX.equalToSuperview()
+            
+            
+        }
+
+        passwordTextField.snp.makeConstraints { make in
+            make.height.equalTo(40)
+            make.width.equalTo(width * 0.7)
+            make.centerX.equalToSuperview()
+            
+
+        }
+
+        loginLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(-28)
+
+            make.height.equalTo(30)
+            make.width.equalTo(85)
+        }
+
+        passwordLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(-28)
+
+            make.height.equalTo(30)
+            make.width.equalTo(85)
+        }
+      
     }
     
     private func setupMainView() {
@@ -94,29 +111,60 @@ class UserLoginViewController: UIViewController, UITextFieldDelegate {
         mainView.layer.rasterizationScale = UIScreen.main.scale
     }
     
+    private func setupStackView() {
+        setupLabels()
+        setupTextFields()
+        
+        userLoginStackView.axis = .vertical
+        userLoginStackView.alignment = .center
+        userLoginStackView.spacing = 25
+        userLoginStackView.distribution = .equalSpacing
+        
+        userLoginStackView.addArrangedSubview(loginTextField)
+        userLoginStackView.addArrangedSubview(passwordTextField)
+    }
+    
     private func setupTextFields() {
-        loginTextField.translatesAutoresizingMaskIntoConstraints = false
-        loginTextField.placeholder = "Title *"
-        loginTextField.layer.borderColor = UIColor.black.cgColor
-        loginTextField.layer.borderWidth = 2
+        loginTextField.placeholder = "Enter your login"
+        loginTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: loginTextField.frame.height))
+        loginTextField.leftViewMode = .always
+        loginTextField.layer.borderColor = UIColor.lightGray.cgColor
+        loginTextField.layer.borderWidth = 1.5
         loginTextField.layer.cornerRadius = 15
         loginTextField.keyboardType = UIKeyboardType.default
         loginTextField.returnKeyType = UIReturnKeyType.done
         loginTextField.autocorrectionType = UITextAutocorrectionType.no
-        loginTextField.font = UIFont.systemFont(ofSize: 13)
-        loginTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        loginTextField.font = UIFont.systemFont(ofSize: 15)
         loginTextField.clearButtonMode = UITextField.ViewMode.whileEditing;
         loginTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        loginTextField.addSubview(loginLabel)
     
         
         passwordTextField.placeholder = "Enter your password"
-        loginTextField.layer.cornerRadius = 15
+        passwordTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: loginTextField.frame.height))
+        passwordTextField.leftViewMode = .always
+        passwordTextField.layer.borderColor = UIColor.lightGray.cgColor
+        passwordTextField.layer.borderWidth = 1.5
+        passwordTextField.layer.cornerRadius = 15
+        passwordTextField.keyboardType = UIKeyboardType.default
+        passwordTextField.returnKeyType = UIReturnKeyType.done
+        passwordTextField.autocorrectionType = UITextAutocorrectionType.no
+        passwordTextField.font = UIFont.systemFont(ofSize: 15)
+        passwordTextField.clearButtonMode = UITextField.ViewMode.whileEditing;
+        passwordTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
         passwordTextField.isSecureTextEntry = true
+        passwordTextField.addSubview(passwordLabel)
+        
     }
     
     private func setupLabels() {
-        loginLabel.text = ""
-        loginLabel.font = UIFont.systemFont(ofSize: 25)
+        loginLabel.text = "Login"
+        loginLabel.font = UIFont.systemFont(ofSize: 18)
+        loginLabel.textAlignment = .center
+        
+        passwordLabel.text = "Password"
+        passwordLabel.font = UIFont.systemFont(ofSize: 18)
+        passwordLabel.textAlignment = .center
     }
 }
 
